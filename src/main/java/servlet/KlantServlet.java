5e8ff552b709b431c36fc54517804b3fc8d71d7e
agung udijana <agung.udijana@gmail.com>
2016-05-27 15:15:58 +0200

    gelukt KlantServlet te runnen vanuit de browser :-) (met GlassFish)

--- src/main/java/servlet/KlantServlet.java
+++ src/main/java/servlet/KlantServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,22 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import main.GenericKlantMenu;
-
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import dao.GenericDao;
-import pojo.Klant;
+
+
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import pojo.*;
+
+
 
 /**
  * Servlet implementation class KlantServlet
  */
 public class KlantServlet extends HttpServlet {
 	
-	private static final Logger logger =  LoggerFactory.getLogger(KlantServlet.class);
 	
 	private static final long serialVersionUID = 1L;
 	public static final String HTML_START="<html><body>";
@@ -46,18 +48,21 @@ public class KlantServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		logger.info("doGet methode wordt aangeroepen");
 		
+		
+		currentSession = getSessionFactory().openSession();
 		List<Klant> klanten = (List<Klant>) getCurrentSession().createQuery("from Klant").list();
-
-		System.out.println("De volgende klanten staan in de Klant tabel :");
-
+	
 		PrintWriter out = response.getWriter();
+		
+		out.println(HTML_START + "<h2>De volgende klanten staan in de Klant tabel :</h2><br/>"+HTML_END);
+	
 		
 		for (Klant k : klanten) {
 			out.println(HTML_START + "-" + k.toString() + HTML_END);
-		}
+		} 
 
+		currentSession.close();
 		
 	}
 
@@ -70,6 +75,30 @@ public class KlantServlet extends HttpServlet {
 	
 	public Session getCurrentSession() {
 		return currentSession;
+	}
+	
+	private static SessionFactory getSessionFactory() {
+		
+
+		Configuration configuration = new Configuration()
+				.addAnnotatedClass(Klant.class)
+				.addAnnotatedClass(Account.class)
+				.addAnnotatedClass(Bestelling.class)
+				.addAnnotatedClass(Factuur.class)
+				.addAnnotatedClass(Adres.class)
+				.addAnnotatedClass(AdresType.class)
+				.addAnnotatedClass(BestelArtikel.class)
+				.addAnnotatedClass(Betaalwijze.class)
+				.addAnnotatedClass(Betaling.class)
+				.addAnnotatedClass(Artikel.class)
+				.addAnnotatedClass(KlantAdres.class)
+				.configure();
+		
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+			.applySettings(configuration.getProperties());
+		
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
 	}
 
 }
